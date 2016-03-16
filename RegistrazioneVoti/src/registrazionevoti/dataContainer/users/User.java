18d9c92cae16fd1247c1776351c1f2694abc14e1
pc <pc@pc-Aspire-3690>
2016-03-16 16:15:40 +0100

    commit iniziale

---
+++ RegistrazioneVoti/src/registrazionevoti/dataContainer/users/User.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registrazionevoti.dataContainer.users;
+
+/**
+ *
+ * @author Caronte
+ */
+public enum User {
+    STUDENT,PROFESSOR,MASTER;
+}
