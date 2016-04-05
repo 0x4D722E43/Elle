a24158ab5bd1f5cde88fd50962f641bdc9cbd969
cl419057 <cl419057@aga35>
2016-04-05 16:47:21 +0200

    commit3

---
+++ RegistrazioneVoti/src/UI/StuUi.java
@@ -0,0 +1,15 @@
+package UI;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cl410671
+ */
+public class StuUi {
+    
+}
