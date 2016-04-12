f67c6dfd811dd2f43938699b919e418599ad27f6
cl410671 <cl410671@aga33>
2016-04-12 15:45:40 +0200

    htmlUi prenotazione Appelli

--- RegistrazioneVoti/src/UI/Student/PrenotazioneAppelli.java
+++ RegistrazioneVoti/src/UI/Student/PrenotazioneAppelli.java
@@ -5,22 +5,46 @@
  */
 package UI.Student;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import registrazionevoti.dataContainer.carriera.Carriera;
+import registrazionevoti.dataContainer.corsi.AppelloStu;
+import registrazionevoti.dataContainer.corsi.Corso;
 
 /**
  *
  * @author cl410671
  */
 public class PrenotazioneAppelli {
-    private String htmlString;
+    private String htmlString="",tableStr;
     PrenotazioneAppelli(Carriera libretto){
-        libretto.getCorsi()
+        for(Corso c:libretto.getCorsi()){
+            String nome= c.getNome();
+            for(AppelloStu a:c.getAppelli()){
+                tableStr += app2html(nome,a.getDescrizione(),a.getData().toGMTString());
+            }
+        }
     }
-    private static String app2html(){
-        String tmp="";
-        return tmp+<;
+    private static String app2html(String corso,String descrizione,String data){
+        String tmp="<tr>";
+        tmp +="<td>"+corso+"</td>";
+        tmp +="<td>"+descrizione+"</td>";
+        tmp +="<td>"+data+"</td>";
+        return tmp+="</tr>";
     }
     public String toHtml(){
+        htmlString.replace("$righe_appelli$", tableStr);
         return htmlString;
+    }
+    private void getTemplate() throws FileNotFoundException, IOException{
+        File htmlTemplateFile = new File("HtmlTemplate/prenotazioneAppelli.html");
+        FileReader fr = new FileReader(htmlTemplateFile);
+        BufferedReader br = new BufferedReader(fr); 
+        while(br.ready()){
+            htmlString += br.readLine();
+        }
     }
 }
