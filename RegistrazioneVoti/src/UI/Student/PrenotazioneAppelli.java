bd949093b28a3db2d4581f9ea9be2e320d320cbc
cl410671 <cl410671@aga33>
2016-04-12 15:23:25 +0200

    fixing vari

---
+++ RegistrazioneVoti/src/UI/Student/PrenotazioneAppelli.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI.Student;
+
+import registrazionevoti.dataContainer.carriera.Carriera;
+
+/**
+ *
+ * @author cl410671
+ */
+public class PrenotazioneAppelli {
+    private String htmlString;
+    PrenotazioneAppelli(Carriera libretto){
+        libretto.getCorsi()
+    }
+    private static String app2html(){
+        String tmp="";
+        return tmp+<;
+    }
+    public String toHtml(){
+        return htmlString;
+    }
+}
