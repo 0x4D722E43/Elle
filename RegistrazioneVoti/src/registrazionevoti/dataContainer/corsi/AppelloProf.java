09ca71f95f171ed3bbb0903ff5260c19b210aa4f
cl410671 <cl410671@aga33>
2016-04-12 14:12:40 +0200

    fixing AppellProf

--- RegistrazioneVoti/src/registrazionevoti/dataContainer/corsi/AppelloProf.java
+++ RegistrazioneVoti/src/registrazionevoti/dataContainer/corsi/AppelloProf.java
@@ -15,9 +15,36 @@ import java.util.HashMap;
  */
 public class AppelloProf extends AppelloStu{
     private ArrayList<String> matIscritti;
-    private HashMap<String,Integer> Voti;//<Matricola,voto>
-    private String nomeMateria;
-    AppelloProf(String descrizione,Date data,String aula){
+    private HashMap<String,Integer> voti;//<Matricola,voto>
+    private String nomeCorso;
+    
+    AppelloProf(String descrizione,Date data,String aula,String nomeCorso){
         super(descrizione,data,aula);
+        this.nomeCorso= nomeCorso;
+        this.matIscritti = new ArrayList<>();
+        this.voti = new HashMap<>();
     }
+    public void setVoto(String matricola,Integer voto) throws Exception{
+        if(this.matIscritti.contains(matricola)){
+            this.voti.put(matricola, voto);
+        }else{
+            throw new Exception("Studente non iscritto");
+        }
+    }
+    public void registraStudente(String matricola){
+        this.matIscritti.add(matricola);
+    }
+
+    public ArrayList<String> getMatIscritti() {
+        return matIscritti;
+    }
+
+    public HashMap<String, Integer> getVoti() {
+        return voti;
+    }
+
+    public String getNomeCorso() {
+        return nomeCorso;
+    }
+    
 }
