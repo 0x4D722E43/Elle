ef1f3f04edef5f03d43c22d16eac27ca8df9d67a
cl410671 <cl410671@aga36>
2016-04-19 14:36:31 +0200

    aggiunte classi

---
+++ RegistrazioneVoti/src/UI/Web/student/WebCtrl.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI.Web.student;
+
+import UI.UICtrl;
+
+/**
+ *
+ * @author cl410671
+ */
+public class WebCtrl extends UICtrl {
+
+    
+}
