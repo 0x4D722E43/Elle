b8a0eecafbf9cbab5a49ac68bbf48f3fbbd48a48
Gabriele <Gabriele@Acero>
2017-01-30 11:48:01 +0100

    create le pagine errore e success per lo studente

---
+++ RegistrazioneVoti/src/main/java/progettoelle/registrazionevoti/controllers/student/StudentNewCourse.java
@@ -0,0 +1,56 @@
+package progettoelle.registrazionevoti.controllers.student;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import progettoelle.registrazionevoti.domain.Course;
+import progettoelle.registrazionevoti.domain.Student;
+import progettoelle.registrazionevoti.repositories.DataLayerException;
+import progettoelle.registrazionevoti.repositories.hibernate.CourseRepositoryHibernate;
+import progettoelle.registrazionevoti.repositories.hibernate.EnrollmentRepositoryHibernate;
+import progettoelle.registrazionevoti.services.EnrollOnCourseService;
+
+@ManagedBean
+@RequestScoped
+public class StudentNewCourse {
+    
+    private final EnrollOnCourseService service = new EnrollOnCourseService(new CourseRepositoryHibernate(), new EnrollmentRepositoryHibernate());
+    
+    @ManagedProperty(value="#{studentSession.student}")
+    private Student student;
+    private DataModel<Course> availableCourses;
+
+    public StudentNewCourse() {
+    
+    }
+    
+    @PostConstruct
+    public void initialize() {
+        try {
+            List<Course> courses = service.getCoursesOnWhichStudentCanEnroll(student);
+            availableCourses = new ListDataModel<>(courses);
+        } catch (DataLayerException ex) {
+            Logger.getLogger(StudentNewCourse.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String enrollOnCourse() {
+        try {
+            Course selectedCourse = availableCourses.getRowData();
+            service.enrollOnCourse(student, selectedCourse);
+            return 
+        } catch (DataLayerException ex) {
+            Logger.getLogger(StudentNewCourse.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    
+
+}
