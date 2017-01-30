e34c00254232a575c2ff02187b9f6429deee87ba
Gabriele <Gabriele@Acero>
2017-01-30 23:32:52 +0100

    creato il controller per gestire gli studenti iscritti al corso

---
+++ RegistrazioneVoti/src/main/java/progettoelle/registrazionevoti/controllers/professor/EnrolledStudentsController.java
@@ -0,0 +1,58 @@
+package progettoelle.registrazionevoti.controllers.professor;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.Flash;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import progettoelle.registrazionevoti.domain.Course;
+import progettoelle.registrazionevoti.domain.Enrollment;
+import progettoelle.registrazionevoti.domain.Student;
+import progettoelle.registrazionevoti.repositories.DataLayerException;
+import progettoelle.registrazionevoti.repositories.hibernate.EnrollmentRepositoryHibernate;
+import progettoelle.registrazionevoti.services.ManageEnrolledStudentsService;
+
+public class EnrolledStudentsController {
+    
+    private final ManageEnrolledStudentsService service = new ManageEnrolledStudentsService(new EnrollmentRepositoryHibernate());
+    
+    @ManagedProperty(value = "#{flash}")
+    private Flash flash;
+    private DataModel<Student> enrolledStudents;
+
+    public EnrolledStudentsController() {
+    
+    }
+    
+    @PostConstruct
+    public void initialize() {
+        try {
+            Course course = (Course)flash.get("course");
+            flash.keep("course");
+            List<Student> results = service.getEnrolledStudents(course);
+            enrolledStudents = new ListDataModel<>(results);
+        } catch (DataLayerException ex) {
+            
+        }
+    }
+
+    public Flash getFlash() {
+        return flash;
+    }
+
+    public void setFlash(Flash flash) {
+        this.flash = flash;
+    }
+
+    public DataModel<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(DataModel<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+    
+}
