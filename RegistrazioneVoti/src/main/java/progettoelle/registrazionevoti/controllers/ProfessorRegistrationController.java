f7bcddbf8e367b56de203abecb1c8a05f102fd4f
Gabriele <Gabriele@Acero>
2017-01-30 03:01:21 +0100

    creato controller per la registrazione del professore

---
+++ RegistrazioneVoti/src/main/java/progettoelle/registrazionevoti/controllers/ProfessorRegistrationController.java
@@ -0,0 +1,93 @@
+package progettoelle.registrazionevoti.controllers;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import org.omnifaces.util.Messages;
+import progettoelle.registrazionevoti.domain.Faculty;
+import progettoelle.registrazionevoti.mail.MailException;
+import progettoelle.registrazionevoti.mail.MockEmailService;
+import progettoelle.registrazionevoti.repositories.DataLayerException;
+import progettoelle.registrazionevoti.repositories.hibernate.FacultyRepositoryHibernate;
+import progettoelle.registrazionevoti.repositories.hibernate.UserRepositoryHibernate;
+import progettoelle.registrazionevoti.services.RegisterProfessorService;
+import progettoelle.registrazionevoti.services.ValidationException;
+
+@ManagedBean
+@RequestScoped
+public class ProfessorRegistrationController {
+    
+    private final RegisterProfessorService service = new RegisterProfessorService(new FacultyRepositoryHibernate(), 
+            new UserRepositoryHibernate(), new MockEmailService());
+    
+    private String email;
+    private String name;
+    private String surname;
+    private Faculty selectedFaculty;
+    private List<Faculty> availableFaculties;
+
+    public ProfessorRegistrationController() {
+    
+    }
+    
+    @PostConstruct
+    public void initialize() {
+        try { 
+            availableFaculties = service.getPossibleFaculties();
+        } catch (DataLayerException ignored) { }
+    }
+
+    public String registerProfessor() {
+        try {
+            service.registerProfessor(email, name, surname, selectedFaculty);
+            return "registration-success?faces-redirect=true";
+        } catch (ValidationException ex) {
+            Messages.addGlobalError(ex.getMessage());
+            return "registration-professor";
+        } catch (DataLayerException | MailException ex) {
+            return "registration-error?faces-redirect=true";
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Faculty getSelectedFaculty() {
+        return selectedFaculty;
+    }
+
+    public void setSelectedFaculty(Faculty selectedFaculty) {
+        this.selectedFaculty = selectedFaculty;
+    }
+
+    public List<Faculty> getAvailableFaculties() {
+        return availableFaculties;
+    }
+
+    public void setAvailableFaculties(List<Faculty> availableFaculties) {
+        this.availableFaculties = availableFaculties;
+    }
+    
+}
